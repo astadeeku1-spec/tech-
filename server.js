407504179bcedb5337cff2f7201ab1314870a299
asta <astadeeku1@gmail.com>
2026-02-27 12:33:57 +0530

    Optimize for global cloud hosting

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { open } = require('sqlite');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
