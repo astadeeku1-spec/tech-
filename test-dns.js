2be3e9e1b8b619c7ea8730d28fd7b2dc13259761
astadeeku_spec <astadeeku1@gmail.com>
2026-03-11 16:06:02 +0530

    chore: remove unwanted files, logs, and sensitive data

--- test-dns.js
+++
@@ -1,9 +0,0 @@
-const dns = require('dns');
-const fs = require('fs');
-dns.lookup('google.com', (err, addresses) => {
-    if (err) {
-        fs.writeFileSync('out3.txt', JSON.stringify({ error: err.message }));
-    } else {
-        fs.writeFileSync('out3.txt', JSON.stringify(addresses));
-    }
-});
